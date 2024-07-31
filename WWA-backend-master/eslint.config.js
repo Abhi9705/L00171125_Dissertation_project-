a4346c528b0b287046a9a6a03a75f8bf69efce64
Abhi9705 <L00171125@atu.ie>
2024-07-31 17:23:45 +0100

    added files

--- WWA-backend-master/eslint.config.js
+++ WWA-backend-master/eslint.config.js
@@ -8,13 +8,7 @@ module.exports = {
       "quotes": ["error", "double"],
       "semi": ["error", "always"],
       "no-console": "off",
-      "indent": ["error", 2],
-      "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-      "eqeqeq": ["error", "always"],
-      "no-trailing-spaces": "error",
-      "curly": ["error", "all"],
-      "prefer-const": "error",
-      "no-var": "error"
+  
     },
     linterOptions: {
       reportUnusedDisableDirectives: true
