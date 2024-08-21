150689d7aaa9db97cca4e892dec91c4ea040b217
Abhi9705 <L00171125@atu.ie>
2024-08-22 00:22:17 +0100

    added files

--- measure_mttr.sh
+++ measure_mttr.sh
@@ -10,19 +10,18 @@ echo "MTTR Start Time: $(date -d @$START_TIME)"
 INSTANCE_ID="i-0609b0b289067d17d"
 
 # Flag to indicate if we have detected the instance going down
-DOWN_DETECTED=false
+DOWN_DETECTED=true
 
 # Wait for the instance to return to the 'running' state
 while true; do
   INSTANCE_STATUS=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].State.Name' --output text)
-  echo "Current instance status: $INSTANCE_STATUS"
   
-  if [ "$INSTANCE_STATUS" == "stopping" ] && [ "$DOWN_DETECTED" == false ]; then
+  if [ "$INSTANCE_STATUS" == "stopping" ] && [ "$DOWN_DETECTED" == true ]; then
     # Record the time when the instance starts shutting down
     DOWN_TIME=$(date +%s)
     echo "Instance started stopping at: $(date -d @$DOWN_TIME)"
-    DOWN_DETECTED=true
-  elif [ "$INSTANCE_STATUS" == "running" ] && [ "$DOWN_DETECTED" == true ]; then
+    DOWN_DETECTED=false
+  elif [ "$INSTANCE_STATUS" == "running" ] && [ "$DOWN_DETECTED" == false ]; then
     # Record the time when the instance is back to running
     END_TIME=$(date +%s)
     echo "Instance is back to running at: $(date -d @$END_TIME)"
